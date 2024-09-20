c06bbe183b646622450f3d2449dd7534a1a6a1f6
oi-so <mail@oioi-2020.com>
2024-09-20 17:36:04 +0900

    Update get.py

--- get.py
+++ get.py
@@ -20,8 +20,8 @@ def get(device_id):
         os.makedirs(dir_name)
 
     token_data = get_json()
-    token = token_data["token"]
-    secret = token_data["secret"]
+    token = os.environ("TOKEN")
+    secret = os.environ("SECRET")
 
     nonce = str(uuid.uuid4())
     t = int(round(time.time() * 1000))
